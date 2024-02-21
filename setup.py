6e0eca12ea31c378c79a26abae5b25dfc9b2c886
Alessandro Valentini <alvalentini@fbk.eu>
2024-02-21 17:31:11 +0100

    Updated CI

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-from setuptools import setup # type: ignore
+from setuptools import setup, find_packages
 
 setup(name='tamerlite',
-      version='0.0.1',
+      version='0.1.0',
       description='Lite version of Tamer',
       author='FBK PSO Unit',
       author_email='tamer@fbk.eu',
       url='tamer.fbk.eu',
-      packages=['tamerlite'],
+      packages=find_packages(),
+      install_requires=['rustamer~=0.1.0'],
       python_requires='>=3.7',
       license='Free For Educational Use',
       classifiers=[
