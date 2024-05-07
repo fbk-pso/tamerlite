78ba1d4c843211cf111ffba68e85ad44b18d58cc
Alessandro Valentini <alvalentini@fbk.eu>
2024-05-07 11:03:37 +0200

    Fixed bug in multiqueue

--- rustamer/src/multiqueue.rs
+++ rustamer/src/multiqueue.rs
@@ -1,4 +1,5 @@
-use std::sync::{Arc, Mutex};
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::time::SystemTime;
 use std::{
     collections::BinaryHeap,
@@ -20,15 +21,21 @@ struct StateContainer {
     expanded: bool,
 }
 
+impl StateContainer {
+    fn set_expanded(&mut self, expanded: bool) -> () {
+        self.expanded = expanded;
+    }
+}
+
 #[derive(Debug, Clone)]
 struct PrioritizedItem {
     heuristic: f64,
-    state_container: Arc<Mutex<StateContainer>>,
+    state_container: Rc<RefCell<StateContainer>>,
 }
 
 impl PartialEq for PrioritizedItem {
     fn eq(&self, other: &Self) -> bool {
-        self.heuristic == other.heuristic && self.state_container.lock().unwrap().state.todo.len() == other.state_container.lock().unwrap().state.todo.len()
+        self.heuristic == other.heuristic && self.state_container.borrow().state.todo.len() == other.state_container.borrow().state.todo.len()
     }
 }
 
@@ -46,7 +53,7 @@ impl Ord for PrioritizedItem {
             std::cmp::Ordering::Greater
         } else if self.heuristic > other.heuristic {
             std::cmp::Ordering::Less
-        } else if self.state_container.lock().unwrap().state.todo.len() < other.state_container.lock().unwrap().state.todo.len() {
+        } else if self.state_container.borrow().state.todo.len() < other.state_container.borrow().state.todo.len() {
             std::cmp::Ordering::Greater
         } else {
             std::cmp::Ordering::Less
@@ -58,7 +65,7 @@ impl Ord for PrioritizedItem {
 pub fn multiqueue_search(ss: &mut SearchSpace, heuristics: Vec<(Heuristic, f64)>, timeout: Option<f32>) -> PyResult<Option<Vec<(Option<String>, String, Option<String>)>>> {
     let start = SystemTime::now();
     let init = ss.initial_state(None)?;
-    let item = PrioritizedItem{heuristic: 0.0, state_container: Arc::new(Mutex::new(StateContainer{state: init, expanded: false})) };
+    let item = PrioritizedItem{heuristic: 0.0, state_container: Rc::new(RefCell::new(StateContainer{state: init, expanded: false})) };
 
     let mut opens = Vec::new();
     for _ in heuristics.iter() {
@@ -87,39 +94,42 @@ pub fn multiqueue_search(ss: &mut SearchSpace, heuristics: Vec<(Heuristic, f64)>
             continue;
         }
         if let Some(current) = open.pop() {
-            let sc = &mut current.state_container.lock().unwrap();
-            if sc.expanded {
-                continue;
+            let successor_states;
+            {
+                let sc = & mut (*(current.state_container)).borrow_mut();
+                if sc.expanded {
+                    continue;
+                }
+                sc.set_expanded(true);
+                let state = &sc.state;
+                if !ss.is_temporal {
+                    closed_set.insert(state.clone());
+                    open_set.remove(state);
+                }
+                states_expanded += 1;
+                counter += 1;
+                if ss.goal_reached(&state, None)? {
+                    println!("Expanded states: {}", states_expanded);
+                    return build_plan(ss, &state);
+                }
+                successor_states = ss.get_successor_states(&state)?;
             }
-            sc.expanded = true;
-            let state = &sc.state;
-            if !ss.is_temporal {
-                closed_set.insert(state.clone());
-                open_set.remove(state);
-            }
-            states_expanded += 1;
-            counter += 1;
-            if ss.goal_reached(&state, None)? {
-                println!("Expanded states: {}", states_expanded);
-                return build_plan(ss, &state);
-            } else {
-                for s in ss.get_successor_states(&state)? {
-                    if open_set.contains(&s) || closed_set.contains(&s) {
-                        continue;
-                    }
-                    if !ss.is_temporal {
-                        open_set.insert(s.clone());
-                    }
-                    let sc = Arc::new(Mutex::new(StateContainer{state: s.clone(), expanded: false}));
-                    for (i, (heuristic, weight)) in heuristics.iter().enumerate() {
-                        let h = heuristic.eval(&s, ss)?;
-                        match h {
-                            Some(v) => {
-                                let f = weight * v + (1.0 - weight) * s.g;
-                                opens[i].push(PrioritizedItem{heuristic: f, state_container: sc.clone()});
-                            },
-                            None => continue,
-                        }
+            for s in successor_states {
+                if open_set.contains(&s) || closed_set.contains(&s) {
+                    continue;
+                }
+                if !ss.is_temporal {
+                    open_set.insert(s.clone());
+                }
+                let sc = Rc::new(RefCell::new(StateContainer{state: s.clone(), expanded: false}));
+                for (i, (heuristic, weight)) in heuristics.iter().enumerate() {
+                    let h = heuristic.eval(&s, ss)?;
+                    match h {
+                        Some(v) => {
+                            let f = weight * v + (1.0 - weight) * s.g;
+                            opens[i].push(PrioritizedItem{heuristic: f, state_container: sc.clone()});
+                        },
+                        None => continue,
                     }
                 }
             }
