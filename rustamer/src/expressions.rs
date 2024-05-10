704d386d5e13c57ed3220b10cda38233ea492c37
Andrea Micheli <amicheli@fbk.eu>
2024-05-10 11:30:10 +0200

    EXPERIMENTAL: added a simple ExpressionManager to HFF to try the idea in #7. It seems to work VERY well

--- rustamer/src/expressions.rs
+++ rustamer/src/expressions.rs
@@ -1,6 +1,7 @@
 use num_rational::BigRational;
 use num::BigInt;
 use pyo3::{prelude::*, exceptions::PyValueError};
+use std::collections::HashMap;
 
 use crate::utils::integer_to_i32;
 
@@ -21,6 +22,44 @@ pub enum ExpressionNode {
     Times(Vec<usize>),
     Div(usize, usize),
 }
+
+#[derive(Clone, Debug)]
+pub struct ExpressionManager {
+    all_expressions: Vec<Vec<ExpressionNode>>,
+    expression2id: HashMap<Vec<ExpressionNode>, usize>
+}
+
+impl ExpressionManager {
+    pub fn new() -> ExpressionManager {
+        ExpressionManager{all_expressions: vec![], expression2id: HashMap::new()}
+    }
+
+    pub fn get(&self, expr: usize) -> Option<&Vec<ExpressionNode>> {
+        if expr < self.all_expressions.len() {
+            Some(&self.all_expressions[expr])
+        }
+        else {
+            None
+        }
+    }
+
+    pub fn force_get(&self, expr: usize) -> &Vec<ExpressionNode> {
+        &self.all_expressions[expr]
+    }
+
+    pub fn put(&mut self, expr:&Vec<ExpressionNode>) -> usize {
+        if let Some(x) = self.expression2id.get(expr) {
+            *x
+        }
+        else {
+            let newid = self.all_expressions.len();
+            self.all_expressions.push(expr.clone());
+            self.expression2id.insert(expr.clone(), newid);
+            newid
+        }
+    }
+}
+
 
 pub fn get_rational_from_expression_node(exp: &ExpressionNode) -> PyResult<BigRational> {
     if let ExpressionNode::Int(v) = exp {
