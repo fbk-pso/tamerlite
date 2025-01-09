d1d8b8ab1567b027d4ea86eba7649057c51d249b
Irene Brugnara <ibrugnara@fbk.eu>
2025-01-09 15:05:21 +0100

    added outer activation function with residual

--- tamerlite/rl_heuristics.py
+++ tamerlite/rl_heuristics.py
@@ -1,6 +1,21 @@
 import math
 import torch
+import torch.nn.functional as F
 import numpy as np
+
+def apply_outer_activation(x, outer_activation_type, reward_signal, delta_h):
+    if outer_activation_type=="none":
+        return x
+    elif reward_signal == "old":
+        if outer_activation_type=="soft":
+            return F.softsign(x)
+        elif outer_activation_type=="hard":
+            return F.hardtanh(x)
+    else:
+        if outer_activation_type=="soft":
+            return -F.softplus(-x)
+        elif outer_activation_type=="hard":
+            return F.hardtanh(x, min_val=-3*delta_h, max_val=0)
 
 
 class RLRank:
@@ -13,6 +28,7 @@ class RLRank:
         self._sym_h = sym_h
         self._delta_h = config.delta_h
         self._reward_signal = config.reward_signal
+        self._outer_activation = config.outer_activation
         self._gamma = config.gamma
 
     def eval(self, state, ss, sym_h=None):
@@ -26,10 +42,13 @@ class RLRank:
             if sym_h is None:
                 return None
             else:
+                sym_h = torch.tensor(sym_h)
                 if self._reward_signal=="new":
                     r -= sym_h + 3*self._delta_h
                 else:
                     r += self._gamma**(sym_h-1)
+                r = apply_outer_activation(r, self._outer_activation, self._reward_signal, self._delta_h)
+        r = float(r)
         if self._reward_signal=="new":
             return -r
         else:
@@ -38,7 +57,7 @@ class RLRank:
     def eval_state_vec(self, state_vec):
         s = np.array([state_vec])
         r = self._model(torch.from_numpy(s).float()).detach()[0]
-        return float(r[0])
+        return r[0]
 
 
 class RLHeuristic:
@@ -51,6 +70,7 @@ class RLHeuristic:
         self._gamma = config.gamma
         self._reward_signal = config.reward_signal
         self._residual = config.residual
+        self._outer_activation = config.outer_activation
         self._sym_h = sym_h
 
     def eval(self, state, ss):
@@ -62,11 +82,13 @@ class RLHeuristic:
             sym_h = self._sym_h.eval(state, ss)
             if sym_h is None:
                 return None
+            sym_h = torch.tensor(sym_h)
             if self._reward_signal=="new":
                 r -= sym_h
-                r = min(0,r)
             else:
                 r += self._gamma**(sym_h-1)
+            r = apply_outer_activation(r, self._outer_activation, self._reward_signal, self._delta_h)
+        r = float(r)
         if self._reward_signal=="old":
             if r == 0:
                 return float(self._delta_h)
@@ -75,10 +97,9 @@ class RLHeuristic:
             else:
                 return float(min(self._delta_h, (math.log(min(1, r), self._gamma)+1)))
         else:
-            return -r
+            return -min(0,r)
 
     def eval_state_vec(self, state_vec):
         s = np.array([state_vec])
         r = self._model(torch.from_numpy(s).float()).detach()[0]
-        r = float(r[0])
-        return r   # min(0, -r)
+        return r[0]   # min(0, -r)
