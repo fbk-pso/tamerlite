fdd592ffe8337a360c075db9500753232fabcafa
Davide Lusuardi <lusuardi@fbk.eu>
2025-03-10 14:29:55 +0100

    fix(search metrics): store values as strings

--- rustamer/src/multiqueue.rs
+++ rustamer/src/multiqueue.rs
@@ -64,7 +64,7 @@ impl Ord for PrioritizedItem {
 
 #[pyfunction]
 #[pyo3(signature = (ss, heuristics, timeout=None))]
-pub fn multiqueue_search(ss: &mut SearchSpace, heuristics: Vec<(Heuristic, f64)>, timeout: Option<f32>) -> PyResult<(Option<Vec<(Option<String>, String, Option<String>)>>, HashMap<String, i32>)> {
+pub fn multiqueue_search(ss: &mut SearchSpace, heuristics: Vec<(Heuristic, f64)>, timeout: Option<f32>) -> PyResult<(Option<Vec<(Option<String>, String, Option<String>)>>, HashMap<String, String>)> {
     let mut metrics = HashMap::new();
     let start = SystemTime::now();
     let init = ss.initial_state(None)?;
@@ -112,8 +112,8 @@ pub fn multiqueue_search(ss: &mut SearchSpace, heuristics: Vec<(Heuristic, f64)>
                 states_expanded += 1;
                 counter += 1;
                 if ss.goal_reached(&state, None)? {
-                    metrics.insert("expanded_states".to_string(), states_expanded);
-                    metrics.insert("goal_depth".to_string(), state.g.floor() as i32);
+                    metrics.insert("expanded_states".to_string(), states_expanded.to_string());
+                    metrics.insert("goal_depth".to_string(), state.g.to_string());
                     return build_plan(ss, &state).map(|plan| (plan, metrics));
                 }
                 successor_states = ss.get_successor_states(&state)?;
@@ -139,6 +139,6 @@ pub fn multiqueue_search(ss: &mut SearchSpace, heuristics: Vec<(Heuristic, f64)>
             }
         }
     }
-    metrics.insert("expanded_states".to_string(), states_expanded);
+    metrics.insert("expanded_states".to_string(), states_expanded.to_string());
     Ok((None, metrics))
 }
