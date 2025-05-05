58b1e2d376f0e8e6d1a0b876432a3ccb02307735
Alessandro Valentini <alvalentini@fbk.eu>
2025-05-05 15:20:03 +0200

    chore: add license and copyright

--- tests/test_python_rust.py
+++ tests/test_python_rust.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2025 PSO Unit, Fondazione Bruno Kessler
+# This file is part of TamerLite.
+#
+# TamerLite is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TamerLite is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
 import unified_planning
 from unified_planning.shortcuts import *
 from unified_planning.engines import PlanGenerationResult, PlanGenerationResultStatus
