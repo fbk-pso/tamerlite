65f186552e363d781f11a294cf0a4b21ddd4a920
Andrea Micheli <amicheli@fbk.eu>
2025-05-15 10:06:43 +0200

    refactor(learnt_heuristics): PTPolicy Net returns a list of values for get_rank and get_heuristic methods insted of the first one

--- tamerlite/rl_heuristics.py
+++ tamerlite/rl_heuristics.py
@@ -20,9 +20,9 @@ import torch
 import numpy as np
 from tamerlite.core.heuristics import Heuristic
 
-
-class RLRank(Heuristic):
-    def __init__(self, state_encoder, model, ModelClass, config, sym_h, cache_value_in_state: bool = False):
+class RLHeuristicBase(Heuristic):
+    def __init__(self, state_encoder, model, ModelClass, config, sym_h,
+                 cache_value_in_state: bool = False):
         super().__init__(cache_value_in_state)
         self._state_encoder = state_encoder
         self._model = ModelClass(state_encoder.state_geometry, config)
@@ -31,55 +31,39 @@ class RLRank(Heuristic):
         self._residual = config.residual
         self._sym_h = sym_h
 
+    def _eval(self, state, ss):
+        if ss.goal_reached(state):
+            return 0
+        state_vec = self._state_encoder.get_state_as_vector(state)
+        if self._residual:
+            sym_h = self._sym_h.eval(state, ss)
+            if sym_h is None:
+                return None
+        else:
+            sym_h = -1
+        return self.eval_state_vec(state_vec, sym_h)
+
+    def eval_state_vec(self, state_vec, sym_h):
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+
+class RLRank(RLHeuristicBase):
     @property
     def name(self):
         return "rlrank"
 
-    def _eval(self, state, ss):
-        if ss.goal_reached(state):
-            return 0
-        state_vec = self._state_encoder.get_state_as_vector(state)
-        if self._residual:
-            sym_h = self._sym_h.eval(state, ss)
-            if sym_h is None:
-                return None
-        else:
-            sym_h = -1
-        return self.eval_state_vec(state_vec, sym_h)
-
     def eval_state_vec(self, state_vec, sym_h):
         s = np.array([state_vec])
         r = self._model.get_rank(torch.from_numpy(s).float(), [sym_h])
-        return r
+        return r[0]
 
 
-class RLHeuristic(Heuristic):
-    def __init__(self, state_encoder, model, ModelClass, config, sym_h, cache_value_in_state: bool = False):
-        super().__init__(cache_value_in_state)
-        self._state_encoder = state_encoder
-        self._model = ModelClass(state_encoder.state_geometry, config)
-        self._model.load_state_dict(torch.load(model))
-        self._model.eval()
-        self._residual = config.residual
-        self._sym_h = sym_h
-
+class RLHeuristic(RLHeuristicBase):
     @property
     def name(self):
         return "rlh"
 
-    def _eval(self, state, ss):
-        if ss.goal_reached(state):
-            return 0
-        state_vec = self._state_encoder.get_state_as_vector(state)
-        if self._residual:
-            sym_h = self._sym_h.eval(state, ss)
-            if sym_h is None:
-                return None
-        else:
-            sym_h = -1
-        return self.eval_state_vec(state_vec, sym_h)
-
     def eval_state_vec(self, state_vec, sym_h):
         s = np.array([state_vec])
         r = self._model.get_heuristic(torch.from_numpy(s).float(), [sym_h])
-        return r
+        return r[0]
