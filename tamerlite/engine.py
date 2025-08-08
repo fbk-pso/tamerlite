47eb5406d35f3d8b3e1afe38e1538fe0d1fe22b8
Irene Brugnara <ibrugnara@fbk.eu>
2025-08-08 13:52:11 +0200

    forgot to remove assertion now that get_rank directly returns the NN output also with last_activation=none

--- tamerlite/engine.py
+++ tamerlite/engine.py
@@ -405,7 +405,6 @@ class TamerLite(
                     encoder.search_space, heuristics, timeout, self._params.early_termination
                 )
             elif isinstance(self._params, EntropyDualQueueParams):
-                assert(self._params.rl_params.other_params.last_activation!="none")
                 astar_h, w = self._get_heuristic(
                     self._params.astar_queue, heuristic, encoder, state_encoder
                 )
